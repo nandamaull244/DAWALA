92b089c38e76e4e498ebda49a921acc3c9a3afb8
Luhung <luhung.tea@gmail.com>
2024-10-13 14:39:31 +0700

    feat: migration table + seeder integration

---
+++ database/migrations/2024_10_13_065624_create_activity_log_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activity_log', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('log_name');
+            $table->string('causer');
+            $table->string('action');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activity_log');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +24,40 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $faker = Faker::create('id_ID');
+
+        DB::table('users')->insert([
+            'nik' => $this->generateNIK(),
+            'password' => Hash::make('admin123'),
+            'full_name' => 'admin',
+            'birth_date' => $faker->date('Y-m-d'),
+            'gender' => $faker->randomElement(['Laki-Laki', 'Perempuan']),
+            'no_kk' => $faker->numerify('################'),
+            'email' => 'admin@example.com',
+            'phone_number' => $faker->phoneNumber,
+            'district_id' => $faker->randomDigitNotNull,
+            'village_id' => $faker->randomDigitNotNull,
+            'rt' => $faker->numerify('##'),
+            'rw' => $faker->numerify('##'),
+            'address' => $faker->address,
+            'role' => 'admin',
+            'registration_type' => 'Admin',
+            'registration_status' => $faker->randomElement(['Process', 'Rejected', 'Completed']),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+
+    private function generateNIK()
+    {
+        $faker = Faker::create('id_ID');
+
+        $yy = $faker->numberBetween(90, 99);
+        $MMdd = $faker->date('md'); 
+        $dd = $faker->numerify('##'); 
+        $ss = $faker->numerify('##'); 
+        $nnnnn = $faker->numerify('#####'); 
+
+        return $yy . $MMdd . $dd . $ss . $nnnnn;
     }
 }
