23c630b316faf649fff02d189d66630e65925e5d
Luhung <luhung.tea@gmail.com>
2024-10-14 14:26:49 +0700

    feat: add new migration table

--- database/migrations/2024_10_13_065645_create_services_table.php
+++ database/migrations/2024_10_13_065645_create_services_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->string('reason');
             $table->string('evidence_image');
             $table->string('kk_image');
