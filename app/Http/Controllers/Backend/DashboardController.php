95681d6b0abd57555895cab5dba1fd2b2f985fb6
Luhung <luhung.tea@gmail.com>
2024-10-31 17:38:32 +0700

    fix: statistic dashboard condition

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -12,6 +12,7 @@ class DashboardController extends Controller
     public function index() {
         if (auth()->user()->role == 'admin') {
             $data = $this->dashboardAdmin();
+            return $data;
             return view("Dashboard.dashboard_admin")->with([
                 'data' => $data
             ]);
@@ -28,12 +29,12 @@ class DashboardController extends Controller
     }
 
     private function dashboardAdmin() {
-        $service = Service::query();
-        $data['incoming_visit'] = $service->where('service_status', 'Not Yet')->count();
-        $data['process_visit'] = $service->where('service_status', 'Process')->count();
-        $data['visit_scheduled'] = $service->where('visit_schedule', '!=', null)->count();
-        $data['document_recieved_visit'] = $service->where('document_recieved_status', 'Recieved')->count();
-        $data['completed_visit'] = $service->where('working_status', '!=', null)->count();
+        $baseQuery = Service::query();
+        $data['incoming_visit'] = (clone $baseQuery)->where('service_status', 'Not Yet')->count();
+        $data['process_visit'] = (clone $baseQuery)->where('service_status', 'Process')->count();
+        $data['visit_scheduled'] = (clone $baseQuery)->whereNotNull('visit_schedule')->count();
+        $data['document_recieved_visit'] = (clone $baseQuery)->where('document_recieved_status', 'Recieved')->count();
+        $data['completed_visit'] = (clone $baseQuery)->where('service_status', 'Completed')->count();
 
         $data['services_by_district'] = DB::table('districts')
                                        ->leftJoin('users', 'districts.id', '=', 'users.district_id')
@@ -57,7 +58,7 @@ class DashboardController extends Controller
                 DB::raw('COUNT(CASE WHEN service_status = "Process" THEN 1 END) as process_visit'),
                 DB::raw('COUNT(CASE WHEN visit_schedule IS NOT NULL THEN 1 END) as visit_scheduled'),
                 DB::raw('COUNT(CASE WHEN document_recieved_status = "Recieved" THEN 1 END) as document_recieved_visit'),
-                DB::raw('COUNT(CASE WHEN working_status IS NOT NULL THEN 1 END) as completed_visit')
+                DB::raw('COUNT(CASE WHEN service_status = "Completed" THEN 1 END) as completed_visit')
             ])
             ->first();
     
