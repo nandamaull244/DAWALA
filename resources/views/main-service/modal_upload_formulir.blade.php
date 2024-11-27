4cc44c0b9cb71d8263c09b5c9a19e138c202410f
Luhung <luhung.tea@gmail.com>
2024-11-27 20:33:28 +0700

    feat: modal formulir indikator

--- resources/views/main-service/modal_upload_formulir.blade.php
+++ resources/views/main-service/modal_upload_formulir.blade.php
@@ -8,34 +8,38 @@
             <div class="modal-body">
                 @if (isset($service))
                     @php
-                        $forms = $service->service_form->pluck('form_path', 'form_type')->toArray();
-                        $f101 = $forms['F1.01'] ?? '';
-                        $f102 = $forms['F1.02'] ?? '';
-                        $f102 = $forms['F1.03'] ?? '';
-                        $f104 = $forms['F1.04'] ?? '';
+                        $forms = $service->service_form;
+                        foreach ($forms as $key => $value) {
+                            switch($value['form_type']) {
+                                case 'F1.01' : $f101 = true; break;
+                                case 'F1.02' : $f102 = true; break;
+                                case 'F1.03' : $f103 = true; break;
+                                case 'F1.04' : $f104 = true; break;
+                            }
+                        }
                     @endphp
                 @endif
                 <p class="text-center mb-4">*Pilih formulir yang akan diupload</p>
                 <div class="d-flex flex-wrap justify-content-center gap-2">
                     <div class="position-relative form-group">
-                        <input type="file" id="f101-input" class="d-none" accept="image/*,.pdf" name="f101_file" data-exist="{{ !empty($f101) ? 'true' : 'false' }}">
-                        <label for="f101-input" class="btn btn-outline-primary mb-0">F1.01 {{ $f102->form_path ?? '' }}</label>
-                        <span id="f101-status" class="position-absolute top-0 end-0 translate-middle p-1 @if (!empty($f101)) bg-success @else bg-danger @endif text-white border border-light rounded-circle"></span>
+                        <input type="file" id="f101-input" class="d-none" accept="image/*,.pdf" name="f101_file" data-exist="{{ isset($f101) ? 'true' : 'false' }}">
+                        <label for="f101-input" class="btn btn-outline-primary mb-0">F1.01</label>
+                        <span id="f101-status" class="position-absolute top-0 end-0 translate-middle p-1 @if (isset($f101)) bg-success @else bg-danger @endif text-white border border-light rounded-circle"></span>
                     </div>
                     <div class="position-relative form-group">
-                        <input type="file" id="f102-input" class="d-none" accept="image/*,.pdf" name="f102_file" required data-exist="{{ !empty($f102) ? 'true' : 'false' }}">
+                        <input type="file" id="f102-input" class="d-none" accept="image/*,.pdf" name="f102_file" required data-exist="{{ isset($f102) ? 'true' : 'false' }}">
                         <label for="f102-input" class="btn btn-outline-primary mb-0">F1.02</label>
-                        <span id="f102-status" class="position-absolute top-0 end-0 translate-middle p-1 @if (!empty($f102)) bg-success @else bg-danger @endif text-white border border-light rounded-circle"></span>
+                        <span id="f102-status" class="position-absolute top-0 end-0 translate-middle p-1 @if (isset($f102)) bg-success @else bg-danger @endif text-white border border-light rounded-circle"></span>
                     </div>
                     <div class="position-relative form-group">
-                        <input type="file" id="f103-input" class="d-none" accept="image/*,.pdf" name="f103_file" data-exist="{{ !empty($f103) ? 'true' : 'false' }}">
+                        <input type="file" id="f103-input" class="d-none" accept="image/*,.pdf" name="f103_file" data-exist="{{ isset($f103) ? 'true' : 'false' }}">
                         <label for="f103-input" class="btn btn-outline-primary mb-0">F1.03</label>
-                        <span id="f103-status" class="position-absolute top-0 end-0 translate-middle p-1 @if (!empty($f103)) bg-success @else bg-danger @endif text-white border border-light rounded-circle"></span>
+                        <span id="f103-status" class="position-absolute top-0 end-0 translate-middle p-1 @if (isset($f103)) bg-success @else bg-danger @endif text-white border border-light rounded-circle"></span>
                     </div>
                     <div class="position-relative form-group">
-                        <input type="file" id="f104-input" class="d-none" accept="image/*,.pdf" name="f104_file" data-exist="{{ !empty($f104) ? 'true' : 'false' }}">
+                        <input type="file" id="f104-input" class="d-none" accept="image/*,.pdf" name="f104_file" data-exist="{{ isset($f104) ? 'true' : 'false' }}">
                         <label for="f104-input" class="btn btn-outline-primary mb-0">F1.04</label>
-                        <span id="f104-status" class="position-absolute top-0 end-0 translate-middle p-1 @if (!empty($f104)) bg-success @else bg-danger @endif text-white border border-light rounded-circle"></span>
+                        <span id="f104-status" class="position-absolute top-0 end-0 translate-middle p-1 @if (isset($f104)) bg-success @else bg-danger @endif text-white border border-light rounded-circle"></span>
                     </div>
                 </div>
 
